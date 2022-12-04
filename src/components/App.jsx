09c92828713089f28d9a569bfaf5c0aa63f52329
AndreyGOIT <ae.borga@gmail.com>
2022-12-04 10:55:17 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,24 @@
+import React, { Component } from 'react';
 import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 
-export const App = () => {
-  return (
-    <div
-    // style={{
-    //   height: '100vh',
-    //   display: 'flex',
-    //   justifyContent: 'center',
-    //   alignItems: 'center',
-    //   fontSize: 40,
-    //   color: '#010101',
-    // }}
-    >
-      <Searchbar />
-      <ImageGallery />
-    </div>
-  );
-};
+export class App extends Component {
+  state = {};
+  render() {
+    return (
+      <div
+      // style={{
+      //   height: '100vh',
+      //   display: 'flex',
+      //   justifyContent: 'center',
+      //   alignItems: 'center',
+      //   fontSize: 40,
+      //   color: '#010101',
+      // }}
+      >
+        <Searchbar />
+        <ImageGallery />
+      </div>
+    );
+  }
+}
