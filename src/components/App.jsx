19621834d01fa442cd96dca0008a74c62996d3fb
AndreyGOIT <ae.borga@gmail.com>
2022-12-04 17:33:25 +0200

    http axios

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
 import { Searchbar } from './Searchbar/Searchbar';
-import { ImageGallery } from './ImageGallery/ImageGallery';
+// import { ImageGallery } from './ImageGallery/ImageGallery';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+// Your API key: 30800169-3713389dad872250f057e0e33
+
+const ImageGallery = ({ imageGallery }) => (
+  <ul>
+    {imageGallery.map(({ objectID, url, title }) => (
+      <li key={objectID}>
+        <a href={url} target="_blank" rel="noreferrer noopener">
+          {title}
+        </a>
+      </li>
+    ))}
+  </ul>
+);
 
 export class App extends Component {
-  state = {};
+  state = {
+    images: [],
+  };
+
+  async componentDidMount() {
+    const response = await axios.get('/search?query=react');
+    this.setState({ images: response.data.hits });
+  }
+
   render() {
+    const { images } = this.state;
     return (
       <div
       // style={{
@@ -17,7 +42,8 @@ export class App extends Component {
       // }}
       >
         <Searchbar />
-        <ImageGallery />
+        {images.length > 0 ? <ImageGallery images={images} /> : null}
+        {/* <ImageGallery /> */}
       </div>
     );
   }
--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,3 +1,18 @@
+// axios.defaults.baseURL = 'https://pixabay.com/api/';
+// Your API key: 30800169-3713389dad872250f057e0e33
+
+// const ImageGalleryItem = ({ imageGallery }) => (
+//   <>
+//     {imageGallery.map(({ (какие параметры?), objectID, url, title }) => (
+//       <li key={objectID}>
+//         <a href={url} target="_blank" rel="noreferrer noopener">
+//           {title}
+//         </a>
+//       </li>
+//     ))}
+//   </>
+// );
+
 <li class="gallery-item">
   <img src="" alt="" />
 </li>;
