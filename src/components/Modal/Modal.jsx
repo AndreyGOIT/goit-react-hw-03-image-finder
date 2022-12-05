567ae437b161a7b1d5d0ace321aad70cfa5fd5c4
AndreyGOIT <ae.borga@gmail.com>
2022-12-05 12:56:31 +0200

    close by click Backdrop

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -7,22 +7,36 @@ export default class Modal extends Component {
   componentDidMount() {
     console.log('Modal componentDidMount');
 
-    window.addEventListener('keydown', e => {
-      console.log(e.code);
-      if (e.code === 'Escape') {
-        console.log('Нажали ESC, нужно закрыть модалку');
-        this.props.onClose();
-      }
-    });
+    window.addEventListener('keydown', this.handleKeyDown);
   }
 
   componentWillUnmount() {
     console.log('Modal conmonentWillUnmount');
+
+    window.removeEventListener('keydown', this.handleKeyDown);
   }
+
+  handleKeyDown = e => {
+    console.log(e.code);
+    if (e.code === 'Escape') {
+      console.log('Нажали ESC, нужно закрыть модалку');
+      this.props.onClose();
+    }
+  };
+
+  handleBackdropClick = event => {
+    // console.log('Кликнули в бекдроп');
+
+    // console.log('currentTarget:', event.currentTarget);
+    // console.log('target:', event.target);
+    if (event.currentTarget === event.target) {
+      this.props.onClose();
+    }
+  };
 
   render() {
     return createPortal(
-      <div className={styles.overlay}>
+      <div className={styles.overlay} onClick={this.handleBackdropClick}>
         <div className={styles.modal}>
           {this.props.children}
           <img src="" alt="" />
